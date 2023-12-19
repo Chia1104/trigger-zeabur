2130e2ac76fc967c36e3b18d7ea82f2e02844f46
Chia1104 <yuyuchia7423@gmail.com>
2023-12-19 16:33:09 +0800

    fix: fix nest life cycle

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TriggerDevModule } from "@trigger.dev/nestjs";
 import { AppController } from "./app.controller";
-import IssueLabelerModule from "./modules/issue-labeler/issue-labeler.module";
+import IssueLabelerController from "./modules/issue-labeler/issue-labeler.controller";
+import GithubModule from "./modules/github/github.module";
+import GithubProvider from "./modules/github/github.provider";
 
 @Module({
   imports: [
-    IssueLabelerModule,
+    GithubModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -21,6 +23,7 @@ import IssueLabelerModule from "./modules/issue-labeler/issue-labeler.module";
       }),
     }),
   ],
-  controllers: [AppController],
+  providers: [GithubProvider],
+  controllers: [AppController, IssueLabelerController],
 })
 export class AppModule {}
--- src/modules/issue-labeler/issue-labeler.controller.ts
+++ src/modules/issue-labeler/issue-labeler.controller.ts
@@ -1,13 +1,50 @@
 import { Controller, Get } from "@nestjs/common";
-import IssueLabelerService from "./issue-labeler.service";
+import { InjectGithub } from "../github/github.provider";
+import { type Github, events } from "@trigger.dev/github";
+import { InjectTriggerDevClient } from "@trigger.dev/nestjs";
+import { type TriggerClient } from "@trigger.dev/sdk";
 
 @Controller("/github/issue-labeler")
 class IssueLabelerController {
-  constructor(private readonly issueLabelerService: IssueLabelerService) {}
+  constructor(
+    @InjectGithub() private readonly github: Github,
+    @InjectTriggerDevClient() private readonly client: TriggerClient
+  ) {
+    this.client.defineJob({
+      id: "github-integration-on-issue-opened",
+      name: "GitHub Integration - On Issue Opened",
+      version: "0.1.0",
+      integrations: { github: this.github },
+      trigger: this.github.triggers.repo({
+        event: events.onIssueOpened,
+        owner: "Chia1104",
+        repo: "trigger-zeabur",
+      }),
+      run: async (payload, io, ctx) => {
+        await io.github.addIssueAssignees("add assignee", {
+          owner: payload.repository.owner.login,
+          repo: payload.repository.name,
+          // @ts-expect-error
+          issueNumber: payload.issue.number,
+          assignees: ["Chia1104"],
+        });
+
+        await io.github.addIssueLabels("add label", {
+          owner: payload.repository.owner.login,
+          repo: payload.repository.name,
+          // @ts-expect-error
+          issueNumber: payload.issue.number,
+          labels: ["bug"],
+        });
+
+        return { payload, ctx };
+      },
+    });
+  }
 
   @Get()
   labelIssues() {
-    return this.issueLabelerService.run();
+    return `Running Trigger.dev with client-id ${this.client.id}`;
   }
 }
 
